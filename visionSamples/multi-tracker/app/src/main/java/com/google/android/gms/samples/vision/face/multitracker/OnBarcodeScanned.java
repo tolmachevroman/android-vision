28b08bb5d7b20ab24fff9c5dcaa8b96bcf5a932c
tolmachevroman <tolmachevroman@gmail.com>
2015-09-23 11:01:57 -0300

    OnBarcodeScanned callback

---
+++ visionSamples/multi-tracker/app/src/main/java/com/google/android/gms/samples/vision/face/multitracker/OnBarcodeScanned.java
@@ -0,0 +1,8 @@
+package com.google.android.gms.samples.vision.face.multitracker;
+
+/**
+ * Created by romantolmachev on 23/9/15.
+ */
+public interface OnBarcodeScanned {
+    void onBarcodeScanned(Object barcode);
+}
